7fa1ae532f212d450828ec0f30cd93fe274325bb
mitchric <mitchric@oregonstate.edu>
2020-12-22 15:22:35 -0500

    add more db methods

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ from pitchfork_classes import *
 mydb = mysql.connector.connect(
     host=config.host,
     user=config.user,
-    password=config.password
+    password=config.password,
 )
 
 db = Database(mydb)
 
-db.show_database()
+db.create_database()
+db.use_database()
+
+db.create_table()
+db.show_tables()
+
+
+
+
+
--- pitchfork_classes.py
+++ pitchfork_classes.py
@@ -4,13 +4,25 @@ class Database:
         self.mycursor = mydb.cursor()
 
     def create_database(self):
-        self.mycursor.execute("CREATE DATABASE pitchfork")
+        self.mycursor.execute("CREATE DATABASE IF NOT EXISTS pitchfork")
 
     def delete_database(self):
-        self.mycursor.execute("DROP DATABASE IF EXISTS \'pitchfork\'")
+        self.mycursor.execute("DROP DATABASE IF EXISTS pitchfork")
         
     def show_database(self): 
         self.mycursor.execute("SHOW DATABASES LIKE \'pitchfork\'")
+
+        for x in self.mycursor:
+            print(x)
+
+    def use_database(self):
+        self.mycursor.execute("USE pitchfork")
+
+    def create_table(self):
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS reviews (name VARCHAR(255), album VARCHAR(255), label VARCHAR(255), score FLOAT(3, 1))")
+
+    def show_tables(self):
+        self.mycursor.execute("SHOW TABLES")
 
         for x in self.mycursor:
             print(x)
