ce00c1fc9f6c957c11a973986af022ee400b4ef5
mitchric <mitchric@oregonstate.edu>
2020-12-24 13:20:57 -0500

    add comment

--- update_certificates.py
+++ update_certificates.py
@@ -1,4 +1,6 @@
 from subprocess import call
 
+# generate fresh certificates for ssl certificate verification
+# prevents URLError
 call('sudo update-ca-certificates --fresh', shell=True)
 call('export SSL_CERT_DIR=/etc/ssl/certs', shell=True)
